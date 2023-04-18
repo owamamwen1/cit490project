0b6b68033cc0f05c37afef04f2a78bf42dcdfcfc
OgunniyiOwamamwen <owamamwen@outlook.com>
2023-04-18 13:37:20 +0200

    week01 setup project

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>week01 - setup</p>
+        <h1> BYUI CIT 490 SENIOR PROJECT</h1>
+        <h3>Food Donation Management</h3>
+      <p>A web application that is useful for the management of food donation and collection activities.</p>
+      <p>Develop by <strong>Owamamwen Ogunniyi</strong></p>
+    </div>
+  );
+}
+
+export default App;
